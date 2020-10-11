92c323acf8f35de60d90a63f53bf898cfb9f79e4
SidineiEder <mmegiattoo@gmail.com>
2020-10-11 18:41:35 -0300

    Atualização de metodo de Loop
    Utilizando o metodo de Loop While, ao invés do Loop For.

--- EstudosEmUnity/LoopFor.cs
+++ EstudosEmUnity/LoopFor.cs
@@ -7,11 +7,21 @@ public class LoopFor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Estudo do Loop For
         //VARIAVEL, CONDICAO, INCREMENTO/DECREMENTO
-        for(int x = 1; x <= 10; x++ ){
-                print("Meu inteiro vale "+ x +".");
+        for(int intFor = 1; intFor <= 10; intFor++ ){
+                print("Estou em um Loop For, e meu inteiro vale "+ intFor +".");
         }
-        print ("Acabou o Loop!");
+        print ("Acabou o Loop For!");
+
+        //Estudo do Loop While
+        int intWhile = 1;
+        while(intWhile <= 10){
+            print("Estou em um Loop While, e meu inteiro vale"+ intWhile +".");
+            intWhile++; // Ou intWhile = intWhile+1;
+        }
+        print ("Acabou o Loop While!");
+
     }
 
     // Update is called once per frame
